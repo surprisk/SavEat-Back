568382967cfccf91d39b98662d4301d35353c9da
Samuel <brosse.samuel91@gmail.com>
2024-04-03 14:34:06 +0200

    Category controller op

--- controllers/controller.category.js
+++ controllers/controller.category.js
@@ -34,15 +34,20 @@ exports.create = (req, res) => {
 }
 
 exports.read = (req, res) => {
-  Category.findAll({
+  Category.findOne({
     where: {
       id: req.params.id
     }
   })
   .then(c => {
-    res.status(201).send({
-      category: c
-    })
+    res.status(c ? 201 : 404).send(c ? 
+      {
+        category: c,
+        message: "Read succeful"
+      }: 
+      {
+        message: "Resource not found"
+      })
   })
   .catch(e => {
     res.status(500).send({
@@ -59,8 +64,19 @@ exports.update = (req, res) => {
     }
   })
   .then(c => {
-    res.status(201).send({
-      category: c
+
+    return c[0] > 0 ?
+      Category.findOne({
+        where: {
+          id: req.params.id
+        }
+      }) :
+      false
+
+  })
+  .then(updated => {
+    return res.status(updated ? 201 : 404).send({
+      message: updated || "Resource not found"
     })
   })
   .catch(e => {
@@ -75,6 +91,13 @@ exports.delete = (req, res) => {
     where: {
       id: req.params.id
     }
+  })
+  .then(c => {
+    const deleted = c > 0;
+
+    return res.status(deleted ? 201 : 404).send({
+      message: deleted ? "Delete succeful" : "Resource not found"
+    })
   })
   .catch(e => {
     res.status(500).send({
